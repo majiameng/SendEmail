47808891be98599b9919398e3aa8239e844a6dc9
马佳萌 <666@majiameng.com>
2018-06-29 15:40:40 +0800

    Update sendmail.php

--- sendmail.php
+++ sendmail.php
@@ -3,16 +3,16 @@
     date_default_timezone_set('PRC');
 	require_once "Smtp.class.php";
 	//******************** 配置信息 ********************************
-	$smtpserver = "smtp.163.com";//SMTP服务器
+	$smtpserver = "smtp.mxhichina.com";//SMTP服务器
 	$smtpserverport =25;//SMTP服务器端口
 	/**
 	* 如果报错 'Trying to smtp.163.com:25 Error: Cannot connenct to relay host smtp.163.com Error: Connection timed out (110) Error: Cannot send email to 879042886@qq.com'
 	* 一般我们配置的smtp服务器端口都是25，不过有的服务器或空间提供商把25端口给禁用了，比如阿里云就给禁用了，这个可以找相应的提供商确认一下。如果真是禁用了25端口，可以采用465端口，这个端口很多主流的邮件服务商像网易邮箱、QQ邮箱、阿里云邮箱也都支持，采用了465端口，织梦后台需要如下这么配置，注意，smtp服务器地址前面一定要加上ssl://，否则还是不可用。
 	*/
-	$smtpusermail = "13241379514@163.com";//SMTP服务器的用户邮箱
+	$smtpusermail = "send@majiameng.com";//SMTP服务器的用户邮箱
 	$smtpemailto = $_POST['toemail'];//发送给谁
-	$smtpuser = "13241379514@163.com";//SMTP服务器的用户帐号，注：部分邮箱只需@前面的用户名
-	$smtppass = "13833886554meng";//SMTP服务器的用户密码
+	$smtpuser = "send@majiameng.com";//SMTP服务器的用户帐号，注：部分邮箱只需@前面的用户名
+	$smtppass = "Ma13146662737";//SMTP服务器的用户密码
 
 
 	$mailtitle = $_POST['title'];//邮件主题
